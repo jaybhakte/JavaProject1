b7a1b785b05bdd55a15cb2b9a4821e85ed3ef178
HPworld <HPworld@LAPTOP-ST4T2AQG>
2020-09-29 18:23:15 +0530

    Initial Commit

---
+++ JavaProject1/src/com/example/jaybhakte/MainApp.java
@@ -0,0 +1,11 @@
+package com.example.jaybhakte;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		Student student = new Student("java","Amravati");
+		System.out.println(student);
+		
+	}
+
+}
