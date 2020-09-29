52d3942b6926f8486bb88d9f448bcc8809bcc073
HPworld <HPworld@LAPTOP-ST4T2AQG>
2020-09-29 18:29:18 +0530

    Adding new Student object

--- JavaProject1/src/com/example/jaybhakte/MainApp.java
+++ JavaProject1/src/com/example/jaybhakte/MainApp.java
@@ -4,7 +4,10 @@ public class MainApp {
 
 	public static void main(String[] args) {
 		Student student = new Student("java","Amravati");
+		Student student1 = new Student();
+		
 		System.out.println(student);
+		System.out.println(student1);
 		
 	}
 
